15203952ff57e17bb75e49888d611558211bd4b3
Moorgal <megaboki@gmail.com>
2023-02-04 01:06:31 +0000

    create variables

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+const formHeading = document.querySelector('#form-heading');
+const searchForm = document.querySelector('#search-form');
+const searchInput = document.querySelector('#search-input');
+const searchButton = document.querySelector('#search-button');
+const theHistory = document.querySelector('#history');
+const today = document.querySelector('#today');
+const forecast = document.querySelector('#forecast');
+
+console.log(formHeading);
+console.log(searchForm);
+console.log(searchInput);
+console.log(searchButton);
+console.log(theHistory);
+console.log(today);
+console.log(forecast);
