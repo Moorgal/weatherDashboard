2a3e4fde4fb8e11adc4f65299c975c848ac641d4
Moorgal <megaboki@gmail.com>
2023-02-04 21:26:10 +0000

    Api key test

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,8 @@
+// get data
+// search data
+// display data
+// save last searches to localStorage
+
 const formHeading = document.querySelector('#form-heading');
 const searchForm = document.querySelector('#search-form');
 const searchInput = document.querySelector('#search-input');
@@ -5,11 +10,12 @@ const searchButton = document.querySelector('#search-button');
 const theHistory = document.querySelector('#history');
 const today = document.querySelector('#today');
 const forecast = document.querySelector('#forecast');
+const ApiKey = '3ac8e7e0b94efb1de4e85813ee49a8f8';
+const ApiUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=' + ApiKey;
 
-console.log(formHeading);
-console.log(searchForm);
-console.log(searchInput);
-console.log(searchButton);
-console.log(theHistory);
-console.log(today);
-console.log(forecast);
+$.ajax({
+  url: ApiUrl,
+  method: 'GET',
+}).then(function (response) {
+  console.log(response);
+});
